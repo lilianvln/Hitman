3e3c50feb7eb6f731ebcde5abf2b7876c73995e2
capucinebrt <capucine.berthet@etu.utc.fr>
2023-06-24 19:54:45 +0200

    phase 2 : STRIPS + début A*

--- Phase2.py
+++ Phase2.py
@@ -1,5 +1,6 @@
 import hitman
 from phase1-LV import *
+from queue import PriorityQueue
 
 # 0 = North
 # 1 = East
@@ -16,34 +17,34 @@ def create_state0(plateau, N, M) :
     wall = []
     suit = []
     piano = []
-    for i in range (N-1) :
-        for j in range (M-1) :
+    for i in range (M-1) :
+        for j in range (N-1) :
             if plateau[i][j] == HC.CIVIL_N :
-                civils.append([(i, j), 0])
+                civils.append([(j, M-1-i), 0])
             elif plateau[i][j] == HC.CIVIL_E :
-                civils.append([(i, j), 1])
+                civils.append([(j, M-1-i), 1])
             elif plateau[i][j] == HC.CIVIL_S :
-                civils.append([(i, j), 2])
+                civils.append([(j, M-1-i), 2])
             elif plateau[i][j] == HC.CIVIL_W :
-                civils.append([(i, j), 3])
+                civils.append([(j, M-1-i), 3])
             if plateau[i][j] == HC.GUARD_N :
-                guards.append([(i, j), 0])
+                guards.append([(j, M-1-i), 0])
             elif plateau[i][j] == HC.GUARD_E :
-                guards.append([(i, j), 1])
+                guards.append([(j, M-1-i), 1])
             elif plateau[i][j] == HC.GUARD_S :
-                guards.append([(i, j), 2])
+                guards.append([(j, M-1-i), 2])
             elif plateau[i][j] == HC.GUARD_W :
-                guards.append([(i, j), 3])
+                guards.append([(j, M-1-i), 3])
             elif plateau[i][j] == HC.TARGET :
-                target.append((i, j))
+                target.append((j, M-1-i))
             elif plateau[i][j] == HC.EMPTY :
-                empty.append((i,j))
+                empty.append((j, M-1-i))
             elif plateau[i][j] == HC.WALL :
-                wall.append((i,j))
+                wall.append((j, M-1-i))
             elif plateau[i][j] == HC.SUIT:
-                suit.append((i, j))
+                suit.append((j, M-1-i))
             elif plateau[i][j] == HC.PIANO_WIRE :
-                piano.append((i,j))
+                piano.append((j, M-1-i))
 
 
     state0 = {"hitman": [(0,0), 0],
@@ -57,70 +58,42 @@ def create_state0(plateau, N, M) :
 
     return state0
 
+#class State :
 
 
 
 
 
 
-def turn_clockwise(state) :
-    orientation = state["hitman"][1]
-    precond = 0<= orientation <=3
-    if not precond :
-        return None
-    orientation +=1
-    if orientation == 4 : orientation = 0
-    state["hitman"][1] = orientation
-    return state
-
-def turn_anti_clockwise(state) :
-    orientation = state["hitman"][1]
-    precond = 0 <= orientation <= 3
-    if not precond:
-        return None
-    orientation -= 1
-    if orientation == -1: orientation = 0
-    state["hitman"][1] = orientation
-    return state
 
 
-def case_ahead(x, y, orientation) :
-    if orientation == (0 or 2) :
-        x2 = x
-        if orientation == 0 :
-            y2 = y+1
-        else :
-            y2 = y-1
-    elif orientation == (1 or 3) :
-        y2 = y
-        if orientation == 1:
-            x2 = y + 1
-        else:
-            x2 = y - 1
-    return x2, y2
 
 
-def move(state, N, M) :
-    x, y = state["hitman"][0]
-    orientation = state["hitman"][1]
-    x2, y2 = case_ahead(x, y, orientation)
-    precond = ((x2, y2) not in state["wall"]) and (0<=x2<M) and (0<=y2<N)
-    if not precond :
-        return None
-    for i in state["guards"] :
-        if i[0] == (x2, y2) :
-            return None
-    state["hitman"][0] = (x2, y2)
-    return state
+"""
+class case :
+    def __init__(self, position, g=0, h=0):
+        self.position = position
+        self.g = g  # Coût du chemin depuis le nœud initial
+        self.h = h  # Estimation du coût restant pour atteindre le nœud final
+        self.f = g + h  # Coût total f = g + h
+        self.parent = None
+def etoile(target, heuristique) :
 
-def kill_target(state) :
-    x, y = state["hitman"][0]
-    xT, yT = state["target"]
-    precond = ((xT, yT)==(x,y)) and (state["piano"] == None)
-    if not precond :
-        return None
-    state["target"] = None
-    return state
+    open = []
+    close = []
+    start = case((0,0), 0, heuristique[0][0])
+    open.append(start)
+    while open :
+        current = open[0]
+        for el in open :
+            if el.f < current.f :
+                current = el
+        if current.position == target :
+            break
+        open.remove(current)
+        close.add(current)
+"""
+
 
 def opposite_number(num):
     if num == 0:
@@ -132,59 +105,233 @@ def opposite_number(num):
     elif num == 3:
         return 1
     return None
-def kill_guard(state) :
-    x, y = state["hitman"][0]
-    orientation = state["hitman"][1]
-    x2, y2 = case_ahead(x, y, orientation)
-    g = False
-    for i in state["guards"] :
-        if i[0] == (x2, y2) :
-            g = True
-            o = i[1]
-    precond = g and (o != opposite_number(o) )
-    if not precond :
-        return None
-    state["gards"].remove([(x2, y2), o])
-    state["empty"].append((x2, y2))
 
-def kill_civil(state) :
-    x, y = state["hitman"][0]
-    orientation = state["hitman"][1]
-    x2, y2 = case_ahead(x, y, orientation)
-    c = False
-    for i in state["civils"] :
-        if i[0] == (x2, y2) :
-            c = True
-            o = i[1]
-    precond = c and (o != opposite_number(o) )
-    if not precond :
-        return None
-    state["civils"].remove([(x2, y2), o])
-    state["empty"].append((x2, y2))
+class Noeud:
+    def __init__(self, state, parent=None, cost_g=0, cost_h=0):
+        self.state = state
+        self.parent = parent
+        self.cost_g = cost_g
+        self.cost_h = cost_h
+        self.cost_f = cost_g + cost_h
 
-def get_suit(state) :
-    x, y = state["hitman"][0]
-    precond = (x,y) == state["suit"][0]
-    if not precond :
-        return None
-    state["empty"].append((x, y))
-    state["suit"] = None
+    def turn_clockwise(self, state):
+        orientation = state["hitman"][1]
+        precond = 0 <= orientation <= 3
+        if not precond:
+            return None
+        orientation += 1
+        if orientation == 4: orientation = 0
+        state["hitman"][1] = orientation
+        return state
 
-def get_piano(state) :
-    x, y = state["hitman"][0]
-    precond = (x, y) == state["piano"][0]
-    if not precond:
-        return None
-    state["empty"].append((x, y))
-    state["piano"] = "got"
+    def turn_anti_clockwise(self, state):
+        orientation = state["hitman"][1]
+        precond = 0 <= orientation <= 3
+        if not precond:
+            return None
+        orientation -= 1
+        if orientation == -1: orientation = 0
+        state["hitman"][1] = orientation
+        return state
 
-def wear_suit(state) :
-    precond = state["suit"] == "got"
-    if not precond:
-        return None
-    state["suit"] == "wear"
+    def case_ahead(self, x, y, orientation):
+        if orientation == (0 or 2):
+            x2 = x
+            if orientation == 0:
+                y2 = y + 1
+            else:
+                y2 = y - 1
+        elif orientation == (1 or 3):
+            y2 = y
+            if orientation == 1:
+                x2 = y + 1
+            else:
+                x2 = y - 1
+        return x2, y2
 
-def goal(state) :
-    if (state["target"] == None) and (state["hitman"][0] == (0,0)) :
+    def move(self, state, N, M):
+        x, y = state["hitman"][0]
+        orientation = state["hitman"][1]
+        x2, y2 = case_ahead(x, y, orientation)
+        precond = ((x2, y2) not in state["wall"]) and (0 <= x2 < M) and (0 <= y2 < N)
+        if not precond:
+            return None
+        for i in state["guards"]:
+            if i[0] == (x2, y2):
+                return None
+        state["hitman"][0] = (x2, y2)
+        return state
+
+    def kill_target(self, state):
+        x, y = state["hitman"][0]
+        xT, yT = state["target"]
+        precond = ((xT, yT) == (x, y)) and (state["piano"] == None)
+        if not precond:
+            return None
+        state["target"] = None
+        return state
+
+
+    def kill_guard(self, state):
+        x, y = state["hitman"][0]
+        orientation = state["hitman"][1]
+        x2, y2 = case_ahead(x, y, orientation)
+        g = False
+        for i in state["guards"]:
+            if i[0] == (x2, y2):
+                g = True
+                o = i[1]
+        precond = g and (o != opposite_number(o))
+        if not precond:
+            return None
+        state["gards"].remove([(x2, y2), o])
+        state["empty"].append((x2, y2))
+
+    def kill_civil(self, state):
+        x, y = state["hitman"][0]
+        orientation = state["hitman"][1]
+        x2, y2 = case_ahead(x, y, orientation)
+        c = False
+        for i in state["civils"]:
+            if i[0] == (x2, y2):
+                c = True
+                o = i[1]
+        precond = c and (o != opposite_number(o))
+        if not precond:
+            return None
+        state["civils"].remove([(x2, y2), o])
+        state["empty"].append((x2, y2))
+
+    def get_suit(self, state):
+        x, y = state["hitman"][0]
+        precond = (x, y) == state["suit"][0]
+        if not precond:
+            return None
+        state["empty"].append((x, y))
+        state["suit"] = None
+
+    def get_piano(self, state):
+        x, y = state["hitman"][0]
+        precond = (x, y) == state["piano"][0]
+        if not precond:
+            return None
+        state["empty"].append((x, y))
+        state["piano"] = "got"
+
+    def wear_suit(self, state):
+        precond = state["suit"] == "got"
+        if not precond:
+            return None
+        state["suit"] == "wear"
+
+
+def goal(state):
+    if (state["target"] == None) and (state["hitman"][0] == (0, 0)):
         return True
     return False
+
+"""
+def heuristique(plateau, N, M, target):
+    x, y = target
+    h = []
+    ligne = 0
+    for i in range(M - 1, 0, -1):
+        for j in range(N - 1):
+            if plateau[i][j] == (HC.WALL or HC.GUARD_E or HC.GUARD_N or HC.GUARD_S or HC.GUARD_W):
+                h[ligne].append((i - y) + abs(j - x))
+            else:
+                h[ligne].append(None)
+        ligne += 1
+    return h
+"""
+
+def heurisitque(state) :
+    return h_turn(state) + h_move(state)
+
+
+def h_turn(state) : # calcul de l'heuristique pour tourner
+    xT, yT = state["target"][0]
+    xH, yH = state["hitman"][0]
+    h = 0
+    if state["hitman"][1] == 0:  # hitman orienté nord
+        if not (xH == xT and yT >= yH) :
+            h += 1
+            if yT < yH :
+                h +=1
+    elif state["hitman"][1] == 1:  # hitman orienté est
+        if not (yH == yT and xT >= xH) :
+            h += 1
+            if xT < xH :
+                h +=1
+    elif state["hitman"][1] == 2:  # hitman orienté sud
+        if not (xH == xT and yT <= yH) :
+            h += 1
+            if yT > yH :
+                h +=1
+    elif state["hitman"][1] == 3:  # hitman orienté ouest
+        if not (yH == yT and xT <= xH) :
+            h += 1
+            if xT > xH :
+                h +=1
+    else :
+        return None
+    return h
+
+def h_move(state) : #calcul de l'heuristique pour avancer
+    xT, yT = state["target"][0]
+    xH, yH = state["hitman"][0]
+    return abs(xT-xH) + abs(yT-yH)
+
+
+def initial_node(state, h, N, M) :
+    node0 = Noeud(create_state0(plateau,N,M))
+    return node0
+
+def generate_children(node) :
+    childrens = []
+    node.turn_clockwise(node.state)
+def astar(node0):
+    open = []
+    closed = []
+
+    open.append(node0)
+
+    while not open.empty():
+        current = open[0]
+        for el in open :
+            if el.cost_f < current.cost_f :
+                current = el
+
+
+
+        if goal(current.state): # l'étant courant est l'état final
+            path = []
+            while current is not None:
+                path.append(current.state)
+                current = current.parent
+            path.reverse()
+            return path
+
+        closed.add(current.state)
+
+        # Générer les nœuds enfants en appliquant les opérations possibles
+        children = generate_children(current)
+
+        for child in children:
+            if child.state in closed_nodes:
+                continue
+
+            cost_g = current_node.cost_g + 1  # Coût du mouvement entre les nœuds est 1 pour cet exemple
+            cost_h = heuristic(child.state)  # Fonction heuristique à implémenter
+            cost_f = cost_g + cost_h
+
+            if child in open_nodes.queue:
+                # Vérifier si le nouvel état a un coût f plus bas
+                # Si c'est le cas, mettre à jour le nœud dans la file de priorité
+                # ...
+
+            else:
+                # Ajouter le nouvel état à la file de priorité
+                # ...
+
+    return None  # Aucun chemin trouv
